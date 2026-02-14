1cace57674be9e251def6bd10ce64bfe86f2651d
kinamura <i.kosuke0801@gmail.com>
2026-02-14 23:30:17 +0900

    fix: move private

--- src/client.hpp
+++ src/client.hpp
@@ -5,15 +5,17 @@
 
 class Client
 {
-	private:
-		int fd_;
-		std::string ip_add_;
 	public:
 		Client(){};
 		int		getFd(){return fd_;}
 		void	setFd(int fd){fd_ = fd;}
 
-		void	setIpAdd(std::string ipadd){ip_add_ = ipadd;}
+		std::string	getIpAdd() {return ip_add_;}
+		void		setIpAdd(std::string ipadd){ip_add_ = ipadd;}
+
+	private:
+		int fd_;
+		std::string ip_add_;
 };
 
 #endif
